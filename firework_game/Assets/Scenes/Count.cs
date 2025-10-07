df63de2ae114a6bf7d17f1b3619719faab21486b
Kitahara7023 <human.kagoshima.naozumi@gmail.com>
2025-10-07 17:57:12 +0900

    [add]CountDown作成中

---
+++ firework_game/Assets/Scenes/Count.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Count : MonoBehaviour
+{
+    public int countdownMinutes = 3;
+    private float countdownSeconds;
+    private Text timeText;
+
+    private void Start()
+    {
+        timeText = GetComponent<Text>();
+        countdownSeconds = countdownMinutes * 60;
+    }
+
+    
+    void Update()
+    {
+        countdownSeconds -= Time.deltaTime;
+        var span = new TimeSpan(0, 0, (int)countdownSeconds);
+        timeText.text = span.ToString(@"mm\:ss");
+
+        if (countdownSeconds <= 0)
+        {
+            // 0•b‚É‚È‚Á‚½‚Æ‚«‚Ìˆ—
+        }
+    }
+}
